500910c2fd70060758dcffcc67f60e35daa8c265
Mrunal Shinde <45140271+munns22@users.noreply.github.com>
2022-09-01 16:11:15 +0530

    Delete examserver directory

--- examserver/src/main/java/repo/RoleaRepository.java
+++
@@ -1,7 +0,0 @@
-package repo;
-
-import com.exam.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleaRepository extends JpaRepository<Role,Long> {
-}
--- examserver/src/main/java/repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package repo;
-
-import com.exam.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    public User findByUsername(String username);
-}
